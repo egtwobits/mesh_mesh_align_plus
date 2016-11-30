505681296595a6a4014fdde0eac7ddf4cbf45b31
Eric Gentry <egtwobits@gmail.com>
2016-11-29 23:17:01 -0500

    Added initial "Quick Align Objects" operator.
    Select any number of objects, hit the "Align Objects" button, and
    each object will be aligned with the target (active) object.

--- mesh_mesh_align_plus.py
+++ mesh_mesh_align_plus.py
@@ -3706,6 +3706,45 @@ class QuickAlignPlanesWholeMesh(AlignPlanesBase):
         return True
 
 
+class QuickAlignObjects(bpy.types.Operator):
+    bl_idname = "maplus.quickalignobjects"
+    bl_label = "Align Objects"
+    bl_description = "Align Objects"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        addon_data = bpy.context.scene.maplus_data
+        prims = addon_data.prim_list
+        previous_mode = bpy.context.active_object.mode
+
+        # Get active (target) transformation matrix components
+        active_mat = bpy.context.active_object.matrix_world
+        active_trs = [
+            mathutils.Matrix.Translation(active_mat.decompose()[0]),
+            active_mat.decompose()[1].to_matrix(),
+            mathutils.Matrix.Scale(active_mat.decompose()[2][0], 4),
+        ]
+        active_trs[1].resize_4x4()
+
+        # Copy the transform components from the target to the current object
+        selected = [item for item in bpy.context.scene.objects if item.select]
+        for item in selected:
+            current_mat = item.matrix_world
+            current_trs = [
+                mathutils.Matrix.Translation(current_mat.decompose()[0]),
+                current_mat.decompose()[1].to_matrix(),
+                mathutils.Matrix.Scale(current_mat.decompose()[2][0], 4),
+            ]
+            current_trs[1].resize_4x4()
+            item.matrix_world = (
+                active_trs[0] *
+                active_trs[1] *
+                current_trs[2]
+            )
+
+        return {'FINISHED'}
+
+
 class CalcLineLength(bpy.types.Operator):
     bl_idname = "maplus.calclinelength"
     bl_label = "Calculate Line Length"
@@ -5727,6 +5766,26 @@ class QuickSMEGUI(bpy.types.Panel):
         sme_mesh_apply_items.operator(
             "maplus.quickscalematchedgewholemesh",
             text="Whole Mesh"
+        )
+
+
+class QuickAlignObjectsGUI(bpy.types.Panel):
+    bl_idname = "quick_align_objects_gui"
+    bl_label = "Quick Align Objects"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "Mesh Align Plus"
+    bl_options = {"DEFAULT_CLOSED"}
+
+    def draw(self, context):
+        layout = self.layout
+        maplus_data_ptr = bpy.types.AnyType(bpy.context.scene.maplus_data)
+        addon_data = bpy.context.scene.maplus_data
+        prims = addon_data.prim_list
+
+        layout.operator(
+                "maplus.quickalignobjects",
+                text="Align Objects"
         )
 
 
